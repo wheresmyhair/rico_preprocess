64ec3fe9c6a88206fba9b756528cfb4a4c993f29
wheresmyhair <jia597005217@gmail.com>
2023-03-13 20:38:05 +0800

    1

--- hierarchy.py
+++ hierarchy.py
@@ -5,4 +5,7 @@ path_file = './data/json/0.json'
 with open(path_file, 'r') as f:
     data = json.load(f)
 
+
 data['activity']['root'].keys()
+
+data['activity']['root']['children'][0]['children'][0].keys()
